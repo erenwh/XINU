a201bb5db120082a279ef07e1b39bb19eeff66d6
HanWang <erenwh@gmail.com>
2018-01-28 17:40:29 -0500

    fix

--- xinu-spring2018/system/main1.c
+++ xinu-spring2018/system/main1.c
@@ -5,15 +5,24 @@
 
 process	main1(void)
 {
+	kprintf("\n(Wang, Han)\n");
+	kprintf("\nwang2786\n");
 	recvclr();
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-	resume(create(onandon, 2048, 30, "onandon", 0, CONSOLE));
+	sleep(1);
+	pid32 pid = create(onandon, 2048, 30, "onandon", 0, CONSOLE));
+	resume(pid);
+	sleep(14);
+	kill(pid);
+	recvclr();
 
 	/* Wait for shell to exit and recreate it */
 
 	while (TRUE) {
-		onandon();
-		sleep(14);
+		receive();
+		sleepms(200);
+		kprintf("\n\nMain process recreating shell\n\n");
+		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
 
 	}
 	return OK;
