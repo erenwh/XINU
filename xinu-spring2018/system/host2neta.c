f3e7fff5b7eba6f7ddd90c70820467f3f388804d
HanWang <erenwh@gmail.com>
2018-02-01 18:41:15 -0500

    add functions

---
+++ xinu-spring2018/system/host2neta.c
@@ -0,0 +1,11 @@
+/**
+ * use in-line assembly, i.e., function asm(), to achieve the same goal. 
+ * Save the function long host2neta(long x) in host2neta.c under system/
+ * Han Wang @ Purdue
+ */
+
+long host2neta(long x) {
+    // TODO
+    return 0;
+}
+
