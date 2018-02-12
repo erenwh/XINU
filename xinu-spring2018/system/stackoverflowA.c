b45d9332baacda42e1062876e6a8fe54f473d391
HanWang <erenwh@gmail.com>
2018-02-12 08:26:43 -0500

    stackoveflowA

--- xinu-spring2018/system/stackoverflowA.c
+++ xinu-spring2018/system/stackoverflowA.c
@@ -4,10 +4,5 @@
 #include <xinu.h>
 void stackoverflowA(void) {
     kprintf("Hacker(pid: %d) is starting\n", currpid);
-    unsigned long *vic_sp, *vic_bp;
-    vic_sp = (unsigned long *) proctab[vic_pid].prstkptr;
-    vic_bp = vic_sp + 2; // move up 2
-    vic_bp = *vic_bp;
-    vic_bp = *vic_bp;
 
 }
