466718b8daec308a2a3a8f7c51311a18924eff34
Han Wang <wang2786@purdue.edu>
2018-02-13 21:21:51 -0500

    lab1 edit

--- xinu-spring2018/system/stackinfo.c
+++ xinu-spring2018/system/stackinfo.c
@@ -4,17 +4,17 @@
 void stackinfo() {
     unsigned long *base_ptr;
     unsigned long *stack_ptr;
-
-    asm (
-            "mov %%ebp, %0"
-            : "=r"(base_ptr) //out
-            );
+    //stack pointer
     asm (
             "mov %%esp, %0"
             : "=r"(stack_ptr) //out
             );
-    kprintf("Base of the stack : Address - 0x%08X, Value - 0x%08X\n", stack_ptr, *stack_ptr);
-    kprintf("Top of the stack : Address - 0x%08X, Value - 0x%08X\n", base_ptr, *base_ptr);
+    //base pointer
+    struct procent *pproc = &proctab[(getpid())];
+    base_ptr = (unsigned long *) pproc->prstkbase;
+
+    kprintf("Base of the stack : Address - 0x%08X, Value - 0x%08X\n", base_ptr, *base_ptr);
+    kprintf("Top of the stack : Address - 0x%08X, Value - 0x%08X\n", stack_ptr, *stack_ptr);
     kprintf("\n");
     return;
 }
