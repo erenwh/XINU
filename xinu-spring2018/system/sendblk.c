ac7014d8ac93b163fed464e49abf888960cf603b
erenwh <erenwh@gmail.com>
2018-04-01 16:41:48 -0400

    update

--- xinu-spring2018/system/sendblk.c
+++ xinu-spring2018/system/sendblk.c
@@ -15,10 +15,7 @@ syscall sendblk(
     intmask mask;          /* Saved interrupt mask		*/
     struct procent *prptr; /* Ptr to process' table entry	*/
     struct procent *prptr_send;
-
-    prptr = &proctab[pid];
-    prptr_send = &proctab[currpid];
-
+ 
     /* interrupt */
     mask = disable();
     if (isbadpid(pid))
@@ -26,15 +23,39 @@ syscall sendblk(
         restore(mask);
         return SYSERR;
     }
+
+    prptr = &proctab[pid];
+    prptr_send = &proctab[currpid];
+
     if ((prptr->prstate == PR_FREE))
     {
         restore(mask);
         return SYSERR;
     }
 
-    if (prptr->prhasmsg) {
+    if (prptr->prhasmsg) { // handle blocking message
         prptr_send->sendblkmsg = msg;
         prptr_send->sendblkflag = TRUE;
-        
+        if (prptr_send->rcpblkflag == FALSE) { // if there is no process waiting
+            insert(currpid, sleepq);//insertd?
+        }
+        prptr_send->prstate == PR_SNDBLK;
+        enqueue(currpid, prptr->sendqueue);
+        resched();
+    } else {
+        prptr->prmsg = msg;     /* Deliver message		*/
+        prptr->prhasmsg = TRUE; /* Indicate message is waiting	*/
     }
+
+    if (prptr->prstate == PR_RECV)
+    {
+        ready(pid);
+    }
+    else if (prptr->prstate == PR_RECTIM)
+    {
+        unsleep(pid);
+        ready(pid);
+    }
+    restore(mask); /* Restore interrupts */
+    return OK;
 }
